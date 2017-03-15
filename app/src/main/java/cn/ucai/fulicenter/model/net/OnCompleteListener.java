78652b5b7e6ad0fca3d9a284ef8630d74269efb9
耿小泰 <clawpo@foxmail.com>
2017-03-15 10:25:17 +0800

    新品的接口和实现类添加泛型

--- app/src/main/java/cn/ucai/fulicenter/model/net/OnCompleteListener.java
+++ app/src/main/java/cn/ucai/fulicenter/model/net/OnCompleteListener.java
@@ -6,6 +6,6 @@ import cn.ucai.fulicenter.model.utils.OkHttpUtils;
  * Created by clawpo on 2017/3/15.
  */
 
-public interface OnCompleteListener extends
-        OkHttpUtils.OnCompleteListener {
+public interface OnCompleteListener<T> extends
+        OkHttpUtils.OnCompleteListener<T> {
 }
