1fc6da332869b0d7c75e21e32621b330929048ae
耿小泰 <clawpo@foxmail.com>
2017-03-16 15:31:38 +0800

    商品详情的数据填充,添加指划和自动轮播的自定义控件

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/GoodsDetailsActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/GoodsDetailsActivity.java
@@ -3,14 +3,23 @@ package cn.ucai.fulicenter.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.model.bean.AlbumsBean;
 import cn.ucai.fulicenter.model.bean.GoodsDetailsBean;
 import cn.ucai.fulicenter.model.net.GoodsModel;
 import cn.ucai.fulicenter.model.net.IGoodsModel;
 import cn.ucai.fulicenter.model.net.OnCompleteListener;
+import cn.ucai.fulicenter.model.utils.CommonUtils;
+import cn.ucai.fulicenter.ui.view.FlowIndicator;
 import cn.ucai.fulicenter.ui.view.MFGT;
+import cn.ucai.fulicenter.ui.view.SlideAutoLoopView;
 
 /**
  * Created by clawpo on 2017/3/16.
@@ -18,12 +27,30 @@ import cn.ucai.fulicenter.ui.view.MFGT;
 public class GoodsDetailsActivity extends AppCompatActivity {
     IGoodsModel model;
     int goodsId = 0;
+    @BindView(R.id.tv_good_name_english)
+    TextView mTvGoodNameEnglish;
+    @BindView(R.id.tv_good_name)
+    TextView mTvGoodName;
+    @BindView(R.id.tv_good_price_shop)
+    TextView mTvGoodPriceShop;
+    @BindView(R.id.tv_good_price_current)
+    TextView mTvGoodPriceCurrent;
+    @BindView(R.id.salv)
+    SlideAutoLoopView mSalv;
+    @BindView(R.id.indicator)
+    FlowIndicator mIndicator;
+    @BindView(R.id.wv_good_brief)
+    WebView mWvGoodBrief;
+
+    GoodsDetailsBean bean;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        goodsId = getIntent().getIntExtra(I.Goods.KEY_GOODS_ID,0);
-        if (goodsId==0){
+        ButterKnife.bind(this);
+        goodsId = getIntent().getIntExtra(I.Goods.KEY_GOODS_ID, 0);
+        if (goodsId == 0) {
             MFGT.finish(GoodsDetailsActivity.this);
             return;
         }
@@ -35,13 +62,51 @@ public class GoodsDetailsActivity extends AppCompatActivity {
         model.loadData(GoodsDetailsActivity.this, goodsId, new OnCompleteListener<GoodsDetailsBean>() {
             @Override
             public void onSuccess(GoodsDetailsBean result) {
-
+                if (result!=null){
+                    bean = result;
+                    showDetails();
+                }
             }
 
             @Override
             public void onError(String error) {
-
+                CommonUtils.showShortToast(error);
             }
         });
+    }
+
+    private void showDetails() {
+        mTvGoodNameEnglish.setText(bean.getGoodsEnglishName());
+        mTvGoodName.setText(bean.getGoodsName());
+        mTvGoodPriceShop.setText(bean.getShopPrice());
+        mTvGoodPriceCurrent.setText(bean.getCurrencyPrice());
+        mSalv.startPlayLoop(mIndicator,getAblumUrl(),getAblumCount());
+        mWvGoodBrief.loadDataWithBaseURL(null,bean.getGoodsBrief(),I.TEXT_HTML,I.UTF_8,null);
+    }
+
+    private int getAblumCount() {
+        if (bean.getProperties()!=null && bean.getProperties().length>0){
+            return bean.getProperties()[0].getAlbums().length;
+        }
+        return 0;
+    }
+
+    private String[] getAblumUrl() {
+        if (bean.getProperties()!=null && bean.getProperties().length>0){
+            AlbumsBean[] albums = bean.getProperties()[0].getAlbums();
+            if (albums!=null && albums.length>0){
+                String[] urls = new String[albums.length];
+                for (int i=0;i<albums.length;i++){
+                    urls[i] = albums[0].getImgUrl();
+                }
+                return urls;
+            }
+        }
+        return null;
+    }
+
+    @OnClick(R.id.backClickArea)
+    public void backClick() {
+        MFGT.finish(GoodsDetailsActivity.this);
     }
 }
