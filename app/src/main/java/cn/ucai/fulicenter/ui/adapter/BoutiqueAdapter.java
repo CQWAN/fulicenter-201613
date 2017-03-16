f54b84b3621c4b37c7acb614d106ad1845f1d788
耿小泰 <clawpo@foxmail.com>
2017-03-16 11:06:22 +0800

    复用新品首页,创建精选二级页面

--- app/src/main/java/cn/ucai/fulicenter/ui/adapter/BoutiqueAdapter.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/adapter/BoutiqueAdapter.java
@@ -1,6 +1,7 @@
 package cn.ucai.fulicenter.ui.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.View;
@@ -14,8 +15,10 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.I;
 import cn.ucai.fulicenter.model.bean.BoutiqueBean;
 import cn.ucai.fulicenter.model.utils.ImageLoader;
+import cn.ucai.fulicenter.ui.activity.BoutiqueChildActivity;
 
 /**
  * Created by clawpo on 2017/3/15.
@@ -39,11 +42,18 @@ public class BoutiqueAdapter extends RecyclerView.Adapter {
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         BoutiqueViewHolder vh = (BoutiqueViewHolder) holder;
-        BoutiqueBean bean = mList.get(position);
+        final BoutiqueBean bean = mList.get(position);
         ImageLoader.downloadImg(mContext,vh.mIvBoutiqueImg,bean.getImageurl());
         vh.mTvBoutiqueTitle.setText(bean.getTitle());
         vh.mTvBoutiqueName.setText(bean.getName());
         vh.mTvBoutiqueDescription.setText(bean.getDescription());
+        vh.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mContext.startActivity(new Intent(mContext,BoutiqueChildActivity.class)
+                .putExtra(I.NewAndBoutiqueGoods.CAT_ID,bean.getId()));
+            }
+        });
     }
 
     @Override
