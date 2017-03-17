4a8fce9bfd8cf4bd14ff151c1ad792a21989f40f
耿小泰 <clawpo@foxmail.com>
2017-03-17 16:30:48 +0800

    分类二级页面的数据排序后button的箭头修改

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/CategoryChildActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/CategoryChildActivity.java
@@ -1,10 +1,13 @@
 package cn.ucai.fulicenter.ui.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
@@ -19,6 +22,11 @@ public class CategoryChildActivity extends AppCompatActivity {
     boolean sortAddTime;
     int sortBy = I.SORT_BY_ADDTIME_DESC;
     NewGoodsFragment mNewGoodsFragment;
+    @BindView(R.id.btn_sort_price)
+    Button mBtnSortPrice;
+    @BindView(R.id.btn_sort_addtime)
+    Button mBtnSortAddtime;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,14 +40,21 @@ public class CategoryChildActivity extends AppCompatActivity {
 
     @OnClick({R.id.btn_sort_price, R.id.btn_sort_addtime})
     public void sortList(View view) {
+        Drawable end = null;
         switch (view.getId()) {
             case R.id.btn_sort_price:
-                sortBy = sortPrice?I.SORT_BY_PRICE_ASC:I.SORT_BY_PRICE_DESC;
-                sortPrice  = !sortPrice;
+                sortBy = sortPrice ? I.SORT_BY_PRICE_ASC : I.SORT_BY_PRICE_DESC;
+                sortPrice = !sortPrice;
+                end = getResources().getDrawable(sortPrice?
+                        R.drawable.arrow_order_up:R.drawable.arrow_order_down);
+                mBtnSortPrice.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,end,null);
                 break;
             case R.id.btn_sort_addtime:
-                sortBy = sortAddTime?I.SORT_BY_ADDTIME_ASC:I.SORT_BY_ADDTIME_DESC;
-                sortAddTime  = !sortAddTime;
+                sortBy = sortAddTime ? I.SORT_BY_ADDTIME_ASC : I.SORT_BY_ADDTIME_DESC;
+                sortAddTime = !sortAddTime;
+                end = getResources().getDrawable(sortAddTime?
+                        R.drawable.arrow_order_up:R.drawable.arrow_order_down);
+                mBtnSortAddtime.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,end,null);
                 break;
         }
         mNewGoodsFragment.sortBy(sortBy);
