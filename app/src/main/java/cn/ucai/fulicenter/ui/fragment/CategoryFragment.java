e3924f3b1d786747bf7a297cecf974910c53fdf3
耿小泰 <clawpo@foxmail.com>
2017-03-17 11:29:22 +0800

    分类首页的用户体验优化

--- app/src/main/java/cn/ucai/fulicenter/ui/fragment/CategoryFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/fragment/CategoryFragment.java
@@ -7,12 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.model.bean.CategoryChildBean;
 import cn.ucai.fulicenter.model.bean.CategoryGroupBean;
@@ -36,12 +38,21 @@ public class CategoryFragment extends Fragment {
     List<List<CategoryChildBean>> childList = new ArrayList<>();
     CategoryAdapter adapter;
     int loadIndex = 0;
+    @BindView(R.id.layout_tips)
+    LinearLayout mLayoutTips;
+    View loadView;
+    View loadFail;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_category, container, false);
         ButterKnife.bind(this, view);
+        loadView = LayoutInflater.from(getContext()).inflate(R.layout.loading, mLayoutTips,false);
+        loadFail = LayoutInflater.from(getContext()).inflate(R.layout.load_fail,mLayoutTips,false);
+        mLayoutTips.addView(loadView);
+        mLayoutTips.addView(loadFail);
+        showDialog(true,false);
         return view;
     }
 
@@ -51,6 +62,7 @@ public class CategoryFragment extends Fragment {
         model = new CategoryModel();
         adapter = new CategoryAdapter(getContext());
         mElvCategory.setAdapter(adapter);
+        mElvCategory.setGroupIndicator(null);
         loadGroupData();
     }
 
@@ -58,48 +70,69 @@ public class CategoryFragment extends Fragment {
         model.loadGroupData(getContext(), new OnCompleteListener<CategoryGroupBean[]>() {
             @Override
             public void onSuccess(CategoryGroupBean[] result) {
-                if (result!=null){
+                if (result != null) {
                     ArrayList<CategoryGroupBean> list = ResultUtils.array2List(result);
                     groupList.clear();
                     groupList.addAll(list);
-                    for (int i=0;i<list.size();i++){
+                    for (int i = 0; i < list.size(); i++) {
                         childList.add(new ArrayList<CategoryChildBean>());
-                        loadChildData(list.get(i).getId(),i);
+                        loadChildData(list.get(i).getId(), i);
                     }
                 }
             }
 
             @Override
             public void onError(String error) {
-
+                showDialog(false,false);
             }
         });
     }
 
-    private void loadChildData(int parentId,final int index){
+    private void loadChildData(int parentId, final int index) {
         model.loadChildData(getContext(), parentId, new OnCompleteListener<CategoryChildBean[]>() {
             @Override
             public void onSuccess(CategoryChildBean[] result) {
                 loadIndex++;
-                if (result!=null){
+                if (result != null) {
                     ArrayList<CategoryChildBean> list = ResultUtils.array2List(result);
-                    childList.set(index,list);
+                    childList.set(index, list);
                 }
-                if (loadIndex==groupList.size()){
-                    adapter.initData(groupList,childList);
-                    L.e(TAG,"load add data....");
+                if (loadIndex == groupList.size()) {
+                    adapter.initData(groupList, childList);
+                    L.e(TAG, "load add data....");
+                    showDialog(false,true);
                 }
             }
 
             @Override
             public void onError(String error) {
                 loadIndex++;
-                L.e(TAG,"onError,loadChildData,"+error);
-                if (loadIndex==groupList.size()){
-                    adapter.initData(groupList,childList);
-                    L.e(TAG,"onError,load add data....");
+                L.e(TAG, "onError,loadChildData," + error);
+                if (loadIndex == groupList.size()) {
+                    adapter.initData(groupList, childList);
+                    L.e(TAG, "onError,load add data....");
+                    showDialog(false,false);
                 }
             }
         });
+    }
+
+    @OnClick(R.id.layout_tips)
+    public void loadAgain(){
+        if (loadFail.getVisibility()==View.VISIBLE){
+            loadGroupData();
+            showDialog(true,false);
+        }
+    }
+
+    private void showDialog(boolean dialog,boolean success){
+        loadView.setVisibility(dialog?View.VISIBLE:View.GONE);
+        if (dialog){
+            loadFail.setVisibility(View.GONE);
+            mLayoutTips.setVisibility(View.VISIBLE);
+        }else{
+            mLayoutTips.setVisibility(success?View.GONE:View.VISIBLE);
+            loadFail.setVisibility(success?View.GONE:View.VISIBLE);
+        }
     }
 }
